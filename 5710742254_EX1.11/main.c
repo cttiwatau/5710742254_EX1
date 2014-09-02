e49823d8e204b857759f105af63d65e1e203e578
tiwat <cttiwatau@gmail.com>
2014-09-02 19:55:57 +0700

    571042254_EX1.11

---
+++ 5710742254_EX1.11/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a;
+    printf("Enter integer number :");
+    scanf("%f",&a);
+    printf("Octal number : %o\n",a);
+    printf("Hexadecimal number : %x",a);
+    return 0;
+}
