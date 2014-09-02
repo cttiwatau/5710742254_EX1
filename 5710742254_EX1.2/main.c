6e62924664c5888fca99c2666bce189327b543ab
tiwat <cttiwatau@gmail.com>
2014-09-02 20:00:52 +0700

    571042254_EX1.2

---
+++ 5710742254_EX1.2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c;
+    printf("Enter Number :");
+    scanf("%d %d %d",&a,&b,&c);
+    printf("\n%d\n%d\n%d\n",a,b,c);
+    return 0;
+}
